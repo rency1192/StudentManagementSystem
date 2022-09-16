5ea33b5455cc3b3b0121fa24e4b11ca8f9a287e9
rency_1192 <20it051@charusat.edu.in>
2022-09-16 19:12:23 +0530

    onchange event

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,20 @@
-import React from "react";
+import React, {useState }from "react";
 import { Card, Container, Button ,Form} from "react-bootstrap";
 
 function Login() {
+  const [username, setUsername] = useState(null);
+  const [email, setEmail] = useState(null);
+  const [password, setPassword] = useState(null);
+  let textChanged = (event) => {
+    if (event.target.name === "username") {
+      setUsername(event.target.value);
+    } else if (event.target.name === "email") {
+      setEmail(event.target.value);
+    } else if (event.target.name === "passwoed") {
+      setPassword(event.target.value);
+    }
+  };
+
   return (
     <div className="my-3">
     <Container>
@@ -14,9 +27,11 @@ function Login() {
             <Form.Group className="mb-3">
               <Form.Label>Username</Form.Label>
               <Form.Control
-                
+                onChange={textChanged}
+                name="username"
+                value={username}
                 type="text"
-                placeholder="Enter id"
+                placeholder="Enter username"
                 autoComplete="off"
                 
               />
@@ -24,9 +39,11 @@ function Login() {
             <Form.Group className="mb-3">
               <Form.Label>Email</Form.Label>
               <Form.Control
-                
+                onChange={textChanged}
+                name="email"
+                value={email}
                 type="text"
-                placeholder="Enter name"
+                placeholder="Enter email"
                 autoComplete="off"
                
               />
@@ -34,9 +51,11 @@ function Login() {
             <Form.Group className="mb-3">
               <Form.Label>Password</Form.Label>
               <Form.Control
-               
+               onChange={textChanged}
+               name="password"
+               value={password}
                 type="text"
-                placeholder="Enter address"
+                placeholder="Enter password"
                 autoComplete="off"
                 
               />
